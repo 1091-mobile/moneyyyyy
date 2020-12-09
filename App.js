fd2e353494c79c330c0102068dc81becb79d65c5
laopeeeee <selina890715@gmail.com>
2020-12-09 16:23:24 +0800

    Revert "sign in up"
    This reverts commit 93de2e135acbbc50bde088daca75d743586ddec8.

--- App.js
+++ App.js
@@ -1,53 +1,35 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from "react";
+//import { View, Text } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//import { createStackNavigator } from '@react-navigation/stack';
 
-import SignIn from './src/account/SignIn';
-import SignUp from './src/account/SignUp';
-import Home from './homescreen';
+import SignIn from "./src/account/SignIn";
+import SignUp from "./src/account/SignUp";
+import HomeScreen from "./homescreen"
 
-const Stack = createStackNavigator();
+//const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function MyStack() {
-  return (
-    <Stack.Navigator
-      initialRouteName="SignUp"
-      screenOptions={{
-        headerTitleAlign: 'center',
-        headerStyle: {
-          backgroundColor: '#7c7877',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-        headerLeft: null
-      }}>
-      <Stack.Screen 
-        name="SignUp" 
-        component={SignUp} 
-        options={{ title: '註冊' }}
-      />       
-      <Stack.Screen 
-        name="SignIn" 
-        component={SignIn} 
-        options={{title: '登入'}}
-      
-      />
-      <Stack.Screen 
-       name="Home" 
-       component={Home} 
-       options={{headerShown: false}}
-       
-      />
-    </Stack.Navigator>
-  );
-}
-
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <MyStack />
+      <Tab.Navigator
+      tabBarOptions={{
+        style: { backgroundColor: "#F0E5DE", shadowColor: "#7c7877" },
+        tabStyle: {},
+        labelStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+          color: "#7c7877",
+          lineHeight: 20,
+        },
+      }}>
+        <Tab.Screen name="SignIn" component={SignIn} />
+        <Tab.Screen name="SingUp" component={SignUp} />
+        <Tab.Screen name="HomeSceen" component={HomeScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+export default App;
