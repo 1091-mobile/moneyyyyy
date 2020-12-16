9c496549ba8d33335103c3631969eab68ceb8d05
shinyu0430 <0430shinyu@gmail.com>
2020-12-16 10:55:45 +0800

    update chart's style

--- src/chart/Chart.js
+++ src/chart/Chart.js
@@ -4,11 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 
+
 function App(){
   
   return (
+<View style={{flex:1}}>
+    <View style={{alignItems: 'center'}}>
+    <Text style={{height:50,marginTop:80,fontSize:30,color: "#7c7877"}}>12月數據圖表</Text>
+    </View>
       
-  <WebView source={{ uri: 'https://codepen.io/0430shinyu/full/NWRdEYb' }} style={{ marginTop: -200 }} />
+      <WebView source={{ uri: 'https://codepen.io/0430shinyu/full/NWRdEYb' }} style={{ marginTop: -300}} />
+    <WebView source={{ uri: 'https://codepen.io/0430shinyu/full/NWRdEYb' }} style={{marginTop: -300}} />
+</View>
   );
 }
 
