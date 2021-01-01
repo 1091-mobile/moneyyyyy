3285510ce2496eee9ba9c38005dd38fbef1ee953
shinyu0430 <0430shinyu@gmail.com>
2021-01-01 12:04:02 +0800

    update the method to get user id and chart style

--- src/screens/ChartScreen/Chart.js
+++ src/screens/ChartScreen/Chart.js
@@ -3,15 +3,17 @@ import {Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Spinner from 'react-native-loading-spinner-overlay';
 import * as firebase from "firebase";
+import { render } from 'react-dom';
 
 //尚未完成功能
-//1.抓取使用者的id:https://www.itread01.com/content/1550483643.html
+//1.靠邀 發現我把月份寫死了 要改
 
 //已完成功能
 //1.圖表前端介面
 //2.圖表連後端資料庫
 //3.onload事件
 //4.webview內容放到地端上
+//5.抓取使用者的id:https://www.itread01.com/content/1550483643.html
 
 
 
@@ -39,23 +41,25 @@ let user = firebase.auth().currentUser;
       marginBottom:10,
       color: "#FFFFFF",
       
-      }}>當月數據圖表（{month}月{user.uid}）</Text>
+      }}>當月數據圖表 :{month}月</Text>
     </View>  
     <WebView 
-    startInLoadingState="true" 
+    injectedJavaScript={'alert("hello")'}
+    javaScriptEnabled={true}
+    //injecterjs一定要搭配onMessage欸 莫名其妙欸 https://github.com/react-native-webview/react-native-webview/issues/1260
+    onMessage={(event)=> Alert.alert(event.nativeEvent.data) }
+    //startInLoadingState="true" 
     renderLoading={displaySpinner}  
     source={require(charturl)} 
     // source={{uri:'https://codepen.io/0430shinyu/full/QWKqjgY'+'?test=2'}}
     //onMessage event is required as well to inject the JavaScript code into the WebView.
-    onMessage={(event) => {}}
-    javaScriptEnabled={true}
-    injectedJavaScript={ `
-    document.getElementById('container').value="${user.uid}";
-    `}
-  
+    // useref="webview"
+    //網路上都沒有看到別人這樣寫，幾乎都用 document...value=? 跟onMessage傳
+    injectedJavaScript={`chart("${user.uid}")`}
      />
 </View>
   );
+
 }
 
 function displaySpinner() {
@@ -64,7 +68,7 @@ function displaySpinner() {
     <Spinner
           visible={true}
           textContent={'努力生成圖表中...'}
-          overlayColor={'rgba(180, 128, 104, 0.48)'}
+          overlayColor={'rgba(207, 206, 206, 0.8)'}
         />
     </View>
   );
